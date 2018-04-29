51b5b50424de33da669dfdc2a3cb996f7af60b71
stretch1414 <jmoser1414@gmail.com>
2018-04-29 00:06:38 -0400

    Finished episodes 1-7

---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Order extends React.Component {
+    render() {
+        return (
+            <div className="order">
+                Order
+                <p>blah</p>
+            </div>
+        );
+    }
+}
+
+export default Order;
