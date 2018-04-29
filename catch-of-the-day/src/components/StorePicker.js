8e083d16bec622b1989366075dd6a637902f7f21
stretch1414 <jmoser1414@gmail.com>
2018-04-29 01:22:39 -0400

    Finished episode 10

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
     render() {
         return (
-            <React.Fragment>
+            <form className="store-selector">
                 {/* comment */}
-                <p>Fish!</p>
-                <form className="store-selector">
-                    <h2>Please Enter A Store:</h2>
-                    <input type="text" required placeholder="Store Name" />
-                    <button type="submit">Visit Store</button>
-                </form>
-            </React.Fragment>
+                <h2>Please Enter A Store:</h2>
+                <input
+                    type="text"
+                    required
+                    placeholder="Store Name"
+                    defaultValue={getFunName()}
+                />
+                <button type="submit">Visit Store</button>
+            </form>
         );
     }
 }
